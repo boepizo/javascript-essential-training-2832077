a481b5a49c5b8dd7664d280f502022a26ba8a3e5
boepizo <77757815+boepizo@users.noreply.github.com>
2024-01-29 22:58:46 +0000

    Pending changes exported from your codespace

--- 01_05/script.js
+++ 01_05/script.js
@@ -7,7 +7,7 @@
 /* Multi-line comment
 See! this line is also commented out! */
 
-const updateBackpack = (update) => {
+const updateBackpack = (update) > {
   let main = document.querySelector("main"); // main is an element
   main.innerHTML = markup(backpack);
   console.info(update);
